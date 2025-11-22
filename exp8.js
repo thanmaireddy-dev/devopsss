7159008f1a72845014c36d6aee183402acc70fba
thanmaireddy-dev <thanmaireddy77@gmail.com>
2025-11-22 06:18:25 +0000

    devv

---
+++ exp8.js
@@ -0,0 +1,25 @@
+/*
+
+kubectl version --client
+kubectl cluster--info
+
+docker login
+
+docker tag image1 thanmaiii/image01:latest
+docker push thanmaiii/image01:latest
+
+
+mkdir thanu
+cd thanu
+
+notepad deployment.yaml
+notepad service.yaml
+dir
+
+kubectl apply -f deployment.yaml
+kubectl appply -f service.yaml
+
+kubectl get pods
+kubectl get svc
+
+*/
